ab4bfab46ca064a0a71a57eb75b4b78bc28fcd0b
meredfern <natredfern@Nats-MacBook-Air.local>
2016-05-12 13:26:08 -0700

    Changes to inventory

--- Inventory.java
+++ Inventory.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
 
-public class Inventory extends MouseAdapter
+public class Inventory extends JComponent
 {
     private Image background;
     private int width, height;
-   
+    int width = 910;
+    int height = 100;
+    public void paint(Graphics2D g,T object,int width,int height)
+    {
+        
+    }
 }
