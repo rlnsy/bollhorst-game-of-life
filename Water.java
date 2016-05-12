05e580a0dafe6ad81a46a016c0fc9378d728bd5d
Rowan Lindsay <rowlindsay@gmail.com>
2016-05-12 13:26:36 -0700

    added water physics

--- Water.java
+++ Water.java
@@ -1,10 +1,30 @@
 public class Water extends WorldElement {
+    
+    private WorldElement platform;
+    
     public Water () {
         super("images/water.png");
     }
     
     public void behave() {
         gravitate();
-        
+        if(platform != null) {
+            if(getX() > platform.getX())
+                setLocation(getX()+1,getY());
+            else
+                setLocation(getX()-1,getY());
+        }
+    }
+    
+    public void gravitate() {
+        boolean canMove = true;
+        for(WorldElement e : getWorld().getElements()) {
+            if(isTouching(e)) {
+                canMove = false;
+                platform = e;
+            }
+        }
+        if(canMove)
+            moveDown(2);
     }
 }
