8cf54910d164c07745291f0fb63643ba6cf203c7
Rowan Lindsay <rowlindsay@gmail.com>
2016-05-11 18:35:45 -0700

    added dirt class

---
+++ Dirt.java
@@ -0,0 +1,9 @@
+public class Dirt extends WorldElement {
+    public Dirt () {
+        super("images/dirt.png");
+    }
+    
+    public void update() {
+        //System.out.println(getHitBox().getWidth() + " " + getHitBox().getHeight());
+    }
+}
