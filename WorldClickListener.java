ef91c38fbc6f4be741de4caf238cb31c6993d6a7
Rowan Lindsay <rowlindsay@gmail.com>
2016-05-17 15:03:58 -0700

    added the ability to click and reposition elements

--- WorldClickListener.java
+++ WorldClickListener.java
@@ -1,12 +1,16 @@
 import java.awt.event.*;
+import java.awt.Point;
 
 public class WorldClickListener extends MouseAdapter implements MouseMotionListener {
     private World world;
     private int currentElementIndex;
+    private WorldElement pickedUp;
+    private boolean movingElement;
     
     public WorldClickListener(World world) {
         this.world = world;
         currentElementIndex = 0;
+        movingElement = false;
     }
     
     public void changeCurrentElement(int elementIndex) {
@@ -14,12 +18,32 @@ public class WorldClickListener extends MouseAdapter implements MouseMotionListe
     }
     
     public void mouseClicked(MouseEvent e) {
-        world.addElement(e.getX(),e.getY(),world.getMenu().getElement(currentElementIndex));
+        if(movingElement) {
+            movingElement = false;
+            pickedUp = null;
+        }
+        else {
+            for(WorldElement elem : world.getElements()) {
+                if(elem.getHitBox().contains(new Point(e.getX(),e.getY()))) {
+                    pickedUp = elem;
+                    movingElement = true;
+                }
+            }
+            world.addElement(e.getX(),e.getY(),world.getMenu().getElement(currentElementIndex));
+        }
     }
     
     public void mouseDragged(MouseEvent e) {
        world.addElement(e.getX(),e.getY(),world.getMenu().getElement(currentElementIndex));
     }
     
-    public void mouseMoved(MouseEvent e) {}
+    public void mouseMoved(MouseEvent e) {
+        if(movingElement && pickedUp != null) {
+            pickedUp.setLocation(e.getX(),e.getY());
+        }
+    }
+    
+    public void setMoveElement(WorldElement e) {
+        pickedUp = e;
+    }
 }
