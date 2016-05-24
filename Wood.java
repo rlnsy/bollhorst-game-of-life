05fdd5698763418d218c62991e10ef75f71b5dae
meredfern <menatboy@gmail.com>
2016-05-24 13:38:43 -0700

    fucked up wood block + larger size

--- Wood.java
+++ Wood.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Wood extends PhysicsElement
 
 {
@@ -5,5 +6,23 @@ public class Wood extends PhysicsElement
         super(true);
     }
     public void behave(){
+        ArrayList<PhysicsElement> woodNeighbours = new ArrayList<PhysicsElement>();
+        for(WorldElement e : getWorld().getElements()) {
+            if(e instanceof Wood)
+                woodNeighbours.add((PhysicsElement)(e));
+        }
+        for(int i = 0; i<woodNeighbours.size(); i++)
+        {
+            PhysicsElement e = woodNeighbours.get(i);
+            // && (this.getY() != e.getY() && this.getX() != e.getY())
+            if((woodNeighbours.size() > 1))
+            {
+                this.setLocation(e.getX(), (e.getY()+this.getHeight()/2));
+            }
+        }
+            /*if (isUnder(Wood))
+            {
+                wood.yPos = other.getY();
+            }*/
+        }
     }
-}
