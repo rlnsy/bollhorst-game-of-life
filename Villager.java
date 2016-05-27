ccc253dfa66436bc20c83ad7ae31306bc55667d0
Rowan Lindsay <rowlindsay@gmail.com>
2016-05-26 20:54:35 -0700

    moved random move to villager class

--- Villager.java
+++ Villager.java
@@ -1,5 +1,6 @@
 public class Villager extends PhysicsElement
 {
+    private double playerMovement;
     public Villager() {
         super(true);
     }
@@ -14,5 +15,26 @@ public class Villager extends PhysicsElement
                 return true;
         }
         return false;
+    }
+    public void randomMove() {
+        double randomNum = Math.random();
+        int numMove = 0;
+        if(randomNum < 0.990) {
+            if(numMove%100 == 0)
+               if((playerMovement == 1 && canMoveRight()) || (playerMovement == -1 && canMoveLeft()))
+                    setLocation((int)(getX() + playerMovement),getY());
+        }
+        else if(randomNum <= 0.993 && randomNum >= 0.990) {
+            playerMovement = 1;
+            numMove++;
+        }
+        else if(randomNum > 0.993 && randomNum <= 0.997) {
+            playerMovement = 0;
+            numMove++;
+        }
+        else if(randomNum > 0.997 && canMoveLeft()) {
+            playerMovement = -1;
+            numMove++;
+        }
     }
 }
