e59cf1314b41eff3a4b4ddcf526278bdce2bf9e9
Rowan Lindsay <rowlindsay@gmail.com>
2016-06-01 15:04:28 -0700

    made small changes to Bollhorst

--- src/game_world_elements/Bollhorst.java
+++ src/game_world_elements/Bollhorst.java
@@ -14,36 +14,41 @@ public class Bollhorst extends PhysicsElement {
     private int velocityLeft = 0;
     private int velocityRight = 0;
     private int velocityUp = 0;
-    private int velocityCount = 0;
+    private int behaveCount = 0;
     
     public Bollhorst() {
         super(true);
         speechBox = ImageReader.readImage(ImageReader.getEffectLocation() + "bollhorst_speech.png");
     }
+    
     public void behave() {
         gravitate();
         moveRight();
-        if(velocityRight > 0 && velocityCount%4 == 0)
+        if(velocityRight > 0 && behaveCount%4 == 0)
             velocityRight--;
         moveLeft();
-        if(velocityLeft > 0 && velocityCount%4 == 0)
+        if(velocityLeft > 0 && behaveCount%4 == 0)
             velocityLeft--;
         jump();
-        if(velocityUp > 0 && velocityCount%2 == 0)
+        if(velocityUp > 0 && behaveCount%2 == 0)
             velocityUp--;
-        velocityCount++;
+        behaveCount++;
     }
+    
     public void draw(Graphics g) {
         super.draw(g);
         if(targetDetected())
             drawMessage(g);
     }
+    
     public void drawMessage(Graphics g) {
         g.drawImage(speechBox,getX()-getWidth()/2-speechBox.getWidth(null),getY() - getHeight()/2,getWorld());
     }
+    
     public Rectangle getDetectionBox () {
         return new Rectangle(getX()-getWidth()/2-DETECT_RADIUS,getY()-getHeight()/2,(2*DETECT_RADIUS)+getWidth(),getHeight());
     }
+    
     public boolean targetDetected() {
         for(WorldElement e : getWorld().getElements()) {
             if(getDetectionBox().contains(e.getLocation()) && (e instanceof Villager || (e instanceof Bollhorst && !e.equals(this))))
@@ -55,18 +60,23 @@ public class Bollhorst extends PhysicsElement {
     public void moveRight() {
         setLocation(getX()+velocityRight,getY());
     }
+    
     public void addRightVelocity(){
         velocityRight += 2;
     }
+    
     public void moveLeft() {
         setLocation(getX()-velocityLeft,getY());
     }
+    
     public void addLeftVelocity(){
         velocityLeft += 2;
     }
+    
     public void jump() {
         setLocation(getX(), getY()-velocityUp);
     }
+    
     public void addUpVelocity(){
         velocityUp += 10;
     }
